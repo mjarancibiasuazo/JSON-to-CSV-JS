bc5049ab979f85224d812e507fe96eb537b1b0bc
Jose Arancibia suazo <mjarancibiasuazo@gmail.com>
2022-10-20 18:01:27 -0300

    último commit 20-10-2022

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 
 
 //REFERENCIA DE LOS ELEMENTOS HTML
-const jsonform = document.querySelector("#jsonform");
-const cvsform  = document.querySelector("#cvsform");
-const bConvert = document.querySelector("#bConvert");
+const jsonForm = document.querySelector("#jsonform");//textarea
+const cvsForm  = document.querySelector("#csvform");//textarea
+const bConvert = document.querySelector("#bConvert");//botón convertir
 
 //Botón convertir
-
-bConvert.addEventListener('click', e => {
+bConvert.addEventListener('click', (e) => {
     convertJSONtoCSV();
 });
 
 function convertJSONtoCSV(){
     let json;
-    let keys = [];
-    let vañues = [];
+    let keys = []; //guardamos todos los nombres de las propiedades.
+    let values = [];//guardamos todos los valores.
 
     //si no es el formato de .JSON vendra el error
     try{
@@ -22,12 +21,37 @@ function convertJSONtoCSV(){
     }catch (error){
         console.log('Formato incorrecto JSON', error);
         alert('Formato incorrecto JSON');
+        return;
     }
 
     if (Array.isArray(json)){
+        //algoritmo del arreglo de objetos
+        json.forEach((item) => {
+            const nkeys = Object.keys(item);//vamos a obtener las propiedades del objeto para la cabeera
 
-        
-        //algoritmo
+            //validamos
+            if(keys.length === 0){
+                keys = [...nkeys];//primera vez iterando
+            }else{
+                if(nkeys.length != keys.length){//si el nuevo arreglo de nkeys es diferente al arreglo de keys
+                    throw new Error('Number of keys are different');//si no tengo keys se termina.
+                    
+                }else{
+                    console.log("OK", nkeys);//mostramos las propiedades
+                }
+            }
+            //definimos filas y extraemos los valores
+            const row = keys.map(k  => {
+                return item[k];
+            });
+            values.push([...row]);
+        });
+
+        console.log(keys, values);
+        values.unshift(keys);
+        const text = values.map((v) => v.join(',')).join('\n');
+        csvform.value = text;
+
     }else {
         alert('No es un arreglo de objetos');
     }
